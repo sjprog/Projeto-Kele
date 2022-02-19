bef1c7887b98548b68905ed83352cb0b76406f44
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-02-19 16:41:23 -0300

    animacao no nome

--- js/script.js
+++ js/script.js
@@ -7,3 +7,32 @@ const container = document.querySelector('.container')
 open.addEventListener('click', () => container.classList.add('show-nav')) 
 
 close.addEventListener('click', () => container.classList.remove('show-nav')) 
+
+
+
+
+
+
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+let text ='Sidney Siqueira'
+let idx = 1
+let speed = 100/ speedEl.value
+
+
+
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx >text.length) {
+      //idx=1
+    }
+
+    setTimeout(writeText, speed)
+
+}
