8e1ecfa90f8cb69d6e4ec54f9b313492b99e7e0a
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-02-21 10:39:16 -0300

    agora falta preencher

--- js/pag02.js
+++ js/pag02.js
@@ -1,9 +1,9 @@
 const sliderContainer = document.querySelector('.slider-container')
-const sliderRight = document.querySelector('.right-slide')
-const sliderLeft = document.querySelector('.left-slide')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
-const slidesLength = sliderRight.querySelectorAll('div').length
+const slidesLength = slideRight.querySelectorAll('div').length
 
 let activeSlideIndex = 0
 
@@ -14,10 +14,15 @@ downButton.addEventListener('click', () => changeSlide('down'))
 
 const changeSlide = (direction) => {
     const sliderHeight = sliderContainer.clientHeight
-    if (direction === 'up') {
+    if(direction === 'up') {
         activeSlideIndex++
-        if (activeSlideIndex > slidesLength - 1) {
+        if(activeSlideIndex > slidesLength - 1) {
             activeSlideIndex = 0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
         }
     }
 
