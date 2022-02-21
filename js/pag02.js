c0eff3824003c0e7f633125657e58a1c611ffeb9
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-02-21 09:54:02 -0300

    js

---
+++ js/pag02.js
@@ -0,0 +1,25 @@
+const sliderContainer = document.querySelector('.slider-container')
+const sliderRight = document.querySelector('.right-slide')
+const sliderLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = sliderRight.querySelectorAll('div').length
+
+let actveSlideIndex = 0
+
+sliderLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    }
+
+    sliderRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+}
